29529876d412c8b76cc658c736cb520edd9a8252
Juan Sanchez <juan.sanchez1192@gmail.com>
2019-05-11 13:51:59 -0500

    add credentials

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const aws = require('aws-sdk')
 const app = express()
 
 const port = process.env.PORT || 80
@@ -7,4 +8,12 @@ app.listen(port, '0.0.0.0', () => console.log(`App started, listening on port ${
 
 app.get('/', (req, res) => {
   return res.json({ message: 'je to fajn!🇨🇿' })
+})
+
+app.get('/list-buckets', async (req, res) => {
+  const s3 = new aws.S3()
+
+  const buckets = await s3.listBuckets().promise()
+
+  return res.json({ buckets })
 })
