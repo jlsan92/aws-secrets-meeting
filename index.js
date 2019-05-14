0e119fb1452687cd2d28d6bc4fa9ab5883d42b04
Juan Sanchez <juan.sanchez1192@gmail.com>
2019-05-14 10:56:58 +0200

    Add travis docker and list bucket endpoint

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const aws = require('aws-sdk')
 const app = express()
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3000
 
 app.listen(port, '0.0.0.0', () => console.log(`App started, listening on port ${port}`))
 
 app.get('/', (req, res) => {
   return res.json({ message: 'je to fajn!🇨🇿' })
+})
+
+app.get('/list-buckets', async (req, res) => {
+  const s3 = new aws.S3()
+
+  const buckets = await s3.listBuckets().promise()
+
+  return res.json({ buckets })
 })
